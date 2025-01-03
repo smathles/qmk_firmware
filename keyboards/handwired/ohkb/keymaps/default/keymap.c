3640237eec4350282d09ddb1c806112808979cd7
Cameron Smith <114649266+smathles@users.noreply.github.com>
2025-01-03 23:18:05 +1000

    keymap progress + json example

--- keyboards/handwired/ohkb/keymaps/default/keymap.c
+++ keyboards/handwired/ohkb/keymaps/default/keymap.c
@@ -5,8 +5,7 @@
 //#define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT(
-        // populate this bad boi
+    [_QWERTY] = LAYOUT_OHKB(
         KC_GRV,   KC_1,     KC_2,     KC_3,       KC_4,     KC_5,            KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,   KC_PSCR,
         KC_TAB,   KC_Q,     KC_W,     KC_E,       KC_R,     KC_T,            KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,   KC_DELETE,
         KC_ESC,   KC_A,     KC_S,     KC_D,       KC_F,     KC_G,            KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,       KC_ENT,        KC_PAGE_UP,
@@ -15,17 +14,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_LCTL,    KC_LGUI,  RAISE,           RAISE,   KC_RGUI, KC_RCTL
     ),
 
-    [_RAISE] = LAYOUT(
+    [_RAISE] = LAYOUT_OHKB(
+        // populate this bad boi
          _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,                KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,
          _______, _______, KC_UP,   _______,  _______, _______,              _______, _______, KC_UP,   _______,  _______, _______, _______, _______,         _______,
-         _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,              _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______,                  _______,
+         KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,              _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______,                  _______,
          _______, _______, _______, _______,  _______, _______,              _______, _______, _______, _______,  _______, _______,                           _______,
          _______, _______, _______,     _______,    _______,                 _______, _______, _______,  _______, _______, _______,
                                     _______,  _______,  _______,             _______, _______, _______
     ),
 };
 
-/*KC_ESC, // make sure you add the KC_CAPS button to the next layer lol
+/*
 KC_HOME,
 KC_END,
 KC_PSCR,
