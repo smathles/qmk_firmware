bf4d47d2ade63f2f30598d8705eeeb35e101f0b2
Cameron Smith <cameronsmith002@gmail.com>
2026-01-25 17:33:38 +1000

    aha it finally compiles

--- keyboards/handwired/ohkb/keymaps/default/keymap.c
+++ keyboards/handwired/ohkb/keymaps/default/keymap.c
@@ -1,11 +1,14 @@
 #include QMK_KEYBOARD_H
-#define _QWERTY 0
+
+enum layer_names {
+    _QWERTY = 0,
+    _RAISE,
+};
 
 #define RAISE MO(_RAISE)
-//#define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_OHKB(
+    [_QWERTY] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PSCR,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      KC_ENT,      KC_PGUP,
@@ -14,8 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LCTL, KC_LGUI, RAISE,           RAISE,   KC_RGUI, KC_RCTL
     ),
 
-    [_RAISE] = LAYOUT_OHKB(
-        // populate this bad boi
+    [_RAISE] = LAYOUT(
          KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
          _______, _______, _______, _______, _______, _______,              KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______,
          KC_CAPS, _______, _______, _______, _______, _______,              KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,      _______,     _______,
@@ -23,14 +25,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______, _______, _______,     _______,    _______,                  _______,    _______,     _______, _______, _______, _______, _______, _______,
                                     _______, _______, _______,              _______, _______, _______
     ),
-
-    // This layer will be a "symbols availability thing"
-    /*[_RAISE] = LAYOUT_OHKB(*/
-    /*     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,*/
-    /*     _______, _______, _______, _______, _______, _______,              KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______,*/
-    /*     KC_CAPS, _______, _______, _______, _______, _______,              KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,      _______,     _______,*/
-    /*     _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______,     _______,      _______, _______,*/
-    /*     _______, _______, _______,     _______,    _______,                  _______,    _______,     _______, _______, _______, _______, _______, _______,*/
-    /*                                _______, _______, _______,              _______, _______, _______*/
-    /*),*/
 };
